4f7fd426e71173ba6a7e49dc3b1a66df7c4accd6
Paul Watson <paulewiii3@gmail.com>
2022-03-08 22:34:08 -0500

    Complete Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,92 @@
 var BinarySearchTree = function(value) {
+  var node = Object.create(binaryTreeMethods);
+  node.root = null;
+  node.value = value;
+  node.left = null;
+  node.right = null;
+  return node;
 };
+
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(value) {
+  var newNode = BinarySearchTree(value);
+  // if (this.root === null) {
+  //   this.root = newNode;
+  //   return this;
+  // } else {
+  //   var current = this.root;
+  //   while (true) {
+  //     if (value === current.value) {
+  //       return undefined;
+  //     }
+  //     if (value < current.value) {
+  //       if (current.left === null) {
+  //         current.left = newNode;
+  //         return this;
+  //       } else {
+  //         current = current.left;
+  //       }
+  //     } else if (value > current.value) {
+  //       if (current.right === null) {
+  //         current.right = newNode;
+  //         return this;
+  //       } else {
+  //         current = current.right;
+  //       }
+  //     }
+  //   }
+  // }
+  var helper = function(tree) {
+    if (value < tree.value) {
+      if (tree.left === null) {
+        tree.left = newNode;
+      } else {
+        helper(tree.left);
+      }
+    } else if (value > tree.value) {
+      if (tree.right === null) {
+        tree.right = newNode;
+      } else {
+        helper(tree.right);
+      }
+    }
+  };
+  helper(this);
+};
+
+binaryTreeMethods.contains = function(value) {
+  if (this.value !== value && this.left === null && this.right === null) {
+    return false;
+  }
+  var current = this;
+  var found = false;
+  while (!found && current) {
+    if (value < current.value) {
+      current = current.left;
+    } else if (value > current.value) {
+      current = current.right;
+    } else {
+      found = true;
+    }
+  }
+  return found;
+};
+
+binaryTreeMethods.depthFirstLog = function(callback) {
+  var helper = function(tree) {
+    callback(tree.value);
+    if (tree.left !== null) {
+      helper(tree.left);
+    }
+    if (tree.right !== null) {
+      helper(tree.right);
+    }
+  };
+  helper(this);
+};
+
 
 
 /*
