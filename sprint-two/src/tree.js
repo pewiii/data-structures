36a02b61330ee3f4167ba6b39cd89dbbc830f4ec
Paul Watson <paulewiii3@gmail.com>
2022-03-08 22:32:04 -0500

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,20 +3,35 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  Object.setPrototypeOf(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var tree = new Tree(value);
+  this.children.push(tree);
 };
 
 treeMethods.contains = function(target) {
+  //var result = false;
+  var childNode = function(tree) {
+    if (tree.value === target) {
+      //result = true;
+      return true;
+    }
+    for (var i = 0; i < tree.children.length; i++) {
+      if (childNode(tree.children[i])) {
+        return true;
+      }
+    }
+    return false;
+  };
+  return childNode(this);
+  //return result;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
